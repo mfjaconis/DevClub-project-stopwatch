b6580438802b227151bf6076fe93e18bf2aa67e2
mfjaconis <matheusjaconis@gmail.com>
2023-09-15 16:42:13 -0300

    Interatividade js

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const timerText = document.querySelector(".text-time")
+const start = document.querySelector(".start")
+const stop = document.querySelector(".stop")
+const restart = document.querySelector(".restart")
+let timerStar
+let timer = 1
+
+function startTime(){
+
+    if(!timerStar){
+        timerStar = true
+
+        timerStar = setInterval(() => {
+             timerText.innerHTML = timer++
+         }, 1000);
+    }
+
+}
+
+function stopTime() {
+    clearInterval(timerStar)
+}
+
+function restartTime() {
+    stopTime()
+    timer = 0
+    timerText.innerHTML = 0
+}
+
+
+start.addEventListener('click', startTime)
+stop.addEventListener('click', stopTime)
+restart.addEventListener('click', restartTime)
