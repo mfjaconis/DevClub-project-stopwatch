723b923d030b211162a02c81929dd28e3ac44a54
mfjaconis <matheusjaconis@gmail.com>
2023-09-18 17:25:04 -0300

    adicionando responsividade, melhoria CSS e adicionando elementos no JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,75 @@
-const timerText = document.querySelector(".text-time")
-const start = document.querySelector(".start")
-const stop = document.querySelector(".stop")
-const restart = document.querySelector(".restart")
-let timerStar
-let timer = 1
+const start = document.querySelector(".start");
+const stop = document.querySelector(".stop");
+const restart = document.querySelector(".restart");
 
-function startTime(){
+let hourElement = document.querySelector("#hour");
+let minuteElement = document.querySelector("#minute");
+let secondElement = document.querySelector("#second");
 
-    if(!timerStar){
-        timerStar = true
+let cronometro;
+let hour = 0;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
 
-        timerStar = setInterval(() => {
-             timerText.innerHTML = timer++
-         }, 1000);
-    }
+function startTime() {
+  stopTime();
 
+  cronometro = setInterval(() => {
+    timer();
+  }, 10);
+
+  start.style.display = "none";
+  stop.style.display = "inline-block";
 }
 
 function stopTime() {
-    clearInterval(timerStar)
-    timerStar = false
+  clearInterval(cronometro);
+
+  start.style.display = "inline-block";
+  stop.style.display = "none";
 }
 
 function restartTime() {
-    stopTime()
-    timer = 0
-    timerText.innerHTML = 0
-    timerStar = false
+  stopTime();
+  hour = 0;
+  minute = 0;
+  second = 0;
+  millisecond = 0;
+
+  hourElement.innerText = "00: ";
+  minuteElement.innerText = "00: ";
+  secondElement.innerText = "00";
+
+  start.style.display = "inline-block";
+  stop.style.display = "none";
 }
 
+function timer() {
+  millisecond += 10;
+  if (millisecond >= 1000) {
+    millisecond = 0;
+    second++;
+  }
+  if (second == 60) {
+    second = 0;
+    minute++;
+  }
+  if (minute == 60) {
+    minute = 0;
+    hour++;
+  }
 
-start.addEventListener('click', startTime)
-stop.addEventListener('click', stopTime)
-restart.addEventListener('click', restartTime)
+  hourElement.innerText = returnData(`${hour}: `);
+  minuteElement.innerText = returnData(`${minute}: `);
+  secondElement.innerText = returnData(`${second}`);
+}
+
+function returnData(input) {
+  return input >= 10 ? input : `0${input}`;
+}
+
+start.addEventListener("click", startTime);
+stop.addEventListener("click", stopTime);
+restart.addEventListener("click", restartTime);
+
